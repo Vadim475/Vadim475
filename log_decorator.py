7a3f8f1c61e2f322b28fdda72770e082379a35dd
Vadym <159933848+Vadim475@users.noreply.github.com>
2024-04-16 18:36:54 +0200

    Add files via upload

---
+++ log_decorator.py
@@ -0,0 +1,45 @@
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def first_decorator(func):
+    def wrapper():
+        print("falling asleep")
+        func()
+        print("wake up")
+    return wrapper
+
+def second_decorator(func):
+    def wrapper():
+        print("Second in")
+        func()
+        print("Second out")
+    return wrapper
+
+def log_decorator(func):
+    def wrapper():
+        start_time = time.time()
+        logger.info(f"Execution started at {start_time}")
+        func()
+        end_time = time.time()
+        logger.info(f"Execution ended at {end_time}")
+        logger.info(f"Execution took {end_time - start_time} seconds")
+    return wrapper
+
+@log_decorator
+@second_decorator
+@first_decorator
+def long_function():
+    time.sleep(5)
+    print("Inside the function")
+
+@log_decorator
+def other_function():
+    time.sleep(2)
+    print("Another function")
+
+if __name__ == "__main__":
+    long_function()
+
